bdefe576c3ab53bed088c7b6fa583d58d40dc7f7
Taklanti <ihab.taklanti@utt.fr>
2019-02-19 16:15:01 +0100

    add top langages

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -5,10 +5,18 @@ class AppController < ApplicationController
   def show
     user_id = Github::User.get_user_id(params[:username])
     @user = Github::User.find(params[:username], user_id)
-    # require 'irb'
-    # ARGV.clear
-    # IRB.start
-    puts @user.to_s
+    top_langs = {}
+    @user.contributions_collection.commit_contributions_by_repository.each do |repository|
+      repository.repository.languages.nodes.each do |lang|
+        if top_langs.key?(lang.name)
+          top_langs[lang.name] = top_langs[lang.name] + 1
+        else
+          top_langs[lang.name] = 1
+        end
+      end
+    end
+    @top_langages = top_langs.sort_by {|k, v| v}.reverse[0..2]
+
   end
 
   skip_before_action :verify_authenticity_token
